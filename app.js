2afaf54d87579caf38b4d58166ca79c7552e1ca6
Enzo Jimenez <35309443+enzojimenez@users.noreply.github.com>
2022-03-07 19:36:54 -0600

    Update app.js

--- app.js
+++ app.js
@@ -28,11 +28,12 @@ app.use((req, res, err) => {
 // listening port
 mongoose
 	.connect(
-		`mongodb+srv://${DB_ADMIN}:${DB_PASSWORD}@10.185.44.100/contacts?retryWrites=true&w=majority`,
-		{
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		}
+		`mongodb://${DB_ADMIN}:${DB_PASSWORD}@10.185.44.100/contacts`,
+		//`mongodb+srv://${DB_ADMIN}:${DB_PASSWORD}@10.185.44.100/contacts?retryWrites=true&w=majority`,
+		//{
+		//	useNewUrlParser: true,
+		//	useUnifiedTopology: true,
+		//}
 	)
 	.then(() => {
 		app.listen(PORT, () => {
